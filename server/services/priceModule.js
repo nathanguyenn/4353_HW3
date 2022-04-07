4cb6a176ac429ec2bf1ca245607993f1932c8b77
nathanguyenn <nathanguyenn@gmail.com>
2022-04-07 13:22:22 -0500

    complete price module

--- server/services/priceModule.js
+++ server/services/priceModule.js
@@ -1,17 +1,23 @@
 class priceModule {
-  static calculate(gallonsRequested) {
-    let customerPricePerGallon = 5.0;
-    const internalPricePerGallon = 1.99;
-    let internalCost = internalPricePerGallon * gallonsRequested;
+  static calculate(gallonsRequested, in_state = false, history = false) {
+    const company_profit = 0.1;
+    const internalPricePerGallon = 1.5;
+    let gallon_factor = gallonsRequested > 1000 ? 0.02 : 0.03;
+    let history_factor = history ? 0.01 : 0;
+    let location_factor = in_state ? 0.02 : 0.04;
+
+    let margin =
+      internalPricePerGallon *
+      (location_factor - history_factor + gallon_factor + company_profit);
+
+    let customerPricePerGallon = internalPricePerGallon + margin;
+
     let totalPrice = customerPricePerGallon * gallonsRequested;
-    let profit = totalPrice - internalCost;
 
     let price = {
       internalPricePerGallon: internalPricePerGallon,
-      internalCost: internalCost,
       customerPricePerGallon: customerPricePerGallon,
       totalPrice: totalPrice,
-      profit: profit,
     };
     return price;
   }
